79b4ae4f6b0c7c33c71c86b4e1cfea7a914e72e9
Manuel Marin <manuelma@kth.se>
2018-08-14 17:35:15 +0200

    Make it a package.

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='spinedatabase_api',
+    version='0.0.1',
+    description='An API to access Spine databases',
+    url='http://github.com/storborg/funniest',
+    author='Manuel Marin',
+    author_email='manuelma@kth.se',
+    license='LGPL',
+    packages=find_packages(),
+    install_requires=[
+          'sqlalchemy',
+      ],
+    zip_safe=False
+)
---
+++ spinedatabase_api/__init__.py
@@ -0,0 +1,3 @@
+from .spinedatabase_api import DatabaseMapping
+
+name = "spinedatabase_api"
