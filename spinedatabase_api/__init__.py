63e010dfb479e8d3715a3e022f410bc8777fb508
Manuel Marin <manuelma@kth.se>
2018-09-26 22:46:04 +0200

    Change temp_ to diff_database_mapping (makes more sense), and remove excessive debug messages

--- spinedatabase_api/__init__.py
+++ spinedatabase_api/__init__.py
@@ -1,5 +1,5 @@
 from .database_mapping import DatabaseMapping
-from .temp_database_mapping import TempDatabaseMapping
+from .diff_database_mapping import DiffDatabaseMapping
 from .exception import SpineDBAPIError, TableNotFoundError, RecordNotFoundError, ParameterValueError
 from .helpers import create_new_spine_database, copy_database, merge_database, is_unlocked, OBJECT_CLASS_NAMES
 
