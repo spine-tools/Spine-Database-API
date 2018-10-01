spine-tools/Spine-Database-API

commit d4a9f07f52e271bccd68cea34839c70b5d785f57

issue #9 Changed session.add_all() to session.bulk_insert_mappings() for all add multiple entities methods

spinedatabase_api/diff_database_mapping.py
@@ -744,14 +744,14 @@ class DiffDatabaseMapping(DatabaseMapping):
             id = max_id + 1 if max_id else 1
         try:
             item_list = list()
+            id_list = set(range(id, len(kwargs_list)))
             for kwargs in kwargs_list:
-                item = self.DiffObjectClass(id=id, **kwargs)
+                item = {"id":id, **kwargs}
                 item_list.append(item)
                 id += 1
-            self.session.add_all(item_list)
+            self.session.bulk_insert_mappings(self.DiffObjectClass, item_list)
             next_id.object_class_id = id
             self.session.commit()
-            id_list = {item.id for item in item_list}
             self.new_item_id["object_class"].update(id_list)
             return self.object_class_list(id_list=id_list)
         except DBAPIError as e:
@@ -773,14 +773,14 @@ class DiffDatabaseMapping(DatabaseMapping):
             id = max_id + 1 if max_id else 1
         try:
             item_list = list()
+            id_list = set(range(id, len(kwargs_list)))
             for kwargs in kwargs_list:
-                item = self.DiffObject(id=id, **kwargs)
+                item = {"id":id, **kwargs}
                 item_list.append(item)
                 id += 1
-            self.session.add_all(item_list)
+            self.session.bulk_insert_mappings(self.DiffObject, item_list)
             next_id.object_id = id
             self.session.commit()
-            id_list = {item.id for item in item_list}
             self.new_item_id["object"].update(id_list)
             return self.object_list(id_list=id_list)
         except DBAPIError as e:
@@ -802,6 +802,7 @@ class DiffDatabaseMapping(DatabaseMapping):
             id = max_id + 1 if max_id else 1
         try:
             item_list = list()
+            id_list = set(range(id, len(kwargs_list)))
             for kwargs in kwargs_list:
                 for dimension, object_class_id in enumerate(kwargs['object_class_id_list']):
                     kwargs = {
@@ -810,13 +811,11 @@ class DiffDatabaseMapping(DatabaseMapping):
                         'object_class_id': object_class_id,
                         'name': kwargs['name']
                     }
-                    item = self.DiffRelationshipClass(**kwargs)
-                    item_list.append(item)
+                    item_list.append(kwargs)
                 id += 1
-            self.session.add_all(item_list)
+            self.session.bulk_insert_mappings(self.DiffRelationshipClass, item_list)
             next_id.relationship_class_id = id
             self.session.commit()
-            id_list = {item.id for item in item_list}
             self.new_item_id["relationship_class"].update(id_list)
             return self.wide_relationship_class_list(id_list=id_list)
         except DBAPIError as e:
@@ -838,6 +837,7 @@ class DiffDatabaseMapping(DatabaseMapping):
             id = max_id + 1 if max_id else 1
         try:
             item_list = list()
+            id_list = set(range(id, len(kwargs_list)))
             for kwargs in kwargs_list:
                 for dimension, object_id in enumerate(kwargs['object_id_list']):
                     kwargs = {
@@ -847,13 +847,11 @@ class DiffDatabaseMapping(DatabaseMapping):
                         'object_id': object_id,
                         'name': kwargs['name']
                     }
-                    item = self.DiffRelationship(**kwargs)
-                    item_list.append(item)
+                    item_list.append(kwargs)
                 id += 1
-            self.session.add_all(item_list)
+            self.session.bulk_insert_mappings(self.DiffRelationship, item_list)
             next_id.relationship_id = id
             self.session.commit()
-            id_list = {item.id for item in item_list}
             self.new_item_id["relationship"].update(id_list)
             return self.wide_relationship_list(id_list=id_list)
         except DBAPIError as e:
@@ -875,14 +873,14 @@ class DiffDatabaseMapping(DatabaseMapping):
             id = max_id + 1 if max_id else 1
         try:
             item_list = list()
+            id_list = set(range(id, len(kwargs_list)))
             for kwargs in kwargs_list:
-                item = self.DiffParameter(id=id, **kwargs)
+                item = {"id":id, **kwargs}
                 item_list.append(item)
                 id += 1
-            self.session.add_all(item_list)
+            self.session.bulk_insert_mappings(self.DiffParameter, item_list)
             next_id.parameter_id = id
             self.session.commit()
-            id_list = {item.id for item in item_list}
             self.new_item_id["parameter"].update(id_list)
             return self.parameter_list(id_list=id_list)
         except DBAPIError as e:
@@ -904,14 +902,14 @@ class DiffDatabaseMapping(DatabaseMapping):
             id = max_id + 1 if max_id else 1
         try:
             item_list = list()
+            id_list = set(range(id, len(kwargs_list)))
             for kwargs in kwargs_list:
-                item = self.DiffParameterValue(id=id, **kwargs)
+                item = {"id":id, **kwargs}
                 item_list.append(item)
                 id += 1
-            self.session.add_all(item_list)
+            self.session.bulk_insert_mappings(self.DiffParameterValue, item_list)
             next_id.parameter_value_id = id
             self.session.commit()
-            id_list = {item.id for item in item_list}
             self.new_item_id["parameter_value"].update(id_list)
             return self.parameter_value_list(id_list=id_list)
         except DBAPIError as e:

commit 5f90c45fc527f71e499c8bd176c101ceaa55415e

issue #9 added replaced add_all with bulk_insert_mappings for commit_session, fixed bug introduced in d4a9f07

spinedatabase_api/diff_database_mapping.py
@@ -744,7 +744,7 @@ class DiffDatabaseMapping(DatabaseMapping):
             id = max_id + 1 if max_id else 1
         try:
             item_list = list()
-            id_list = set(range(id, len(kwargs_list)))
+            id_list = set(range(id, id + len(kwargs_list)))
             for kwargs in kwargs_list:
                 item = {"id":id, **kwargs}
                 item_list.append(item)
@@ -773,7 +773,7 @@ class DiffDatabaseMapping(DatabaseMapping):
             id = max_id + 1 if max_id else 1
         try:
             item_list = list()
-            id_list = set(range(id, len(kwargs_list)))
+            id_list = set(range(id, id + len(kwargs_list)))
             for kwargs in kwargs_list:
                 item = {"id":id, **kwargs}
                 item_list.append(item)
@@ -802,7 +802,7 @@ class DiffDatabaseMapping(DatabaseMapping):
             id = max_id + 1 if max_id else 1
         try:
             item_list = list()
-            id_list = set(range(id, len(kwargs_list)))
+            id_list = set(range(id, id + len(kwargs_list)))
             for kwargs in kwargs_list:
                 for dimension, object_class_id in enumerate(kwargs['object_class_id_list']):
                     kwargs = {
@@ -837,7 +837,7 @@ class DiffDatabaseMapping(DatabaseMapping):
             id = max_id + 1 if max_id else 1
         try:
             item_list = list()
-            id_list = set(range(id, len(kwargs_list)))
+            id_list = set(range(id, id + len(kwargs_list)))
             for kwargs in kwargs_list:
                 for dimension, object_id in enumerate(kwargs['object_id_list']):
                     kwargs = {
@@ -873,7 +873,7 @@ class DiffDatabaseMapping(DatabaseMapping):
             id = max_id + 1 if max_id else 1
         try:
             item_list = list()
-            id_list = set(range(id, len(kwargs_list)))
+            id_list = set(range(id, id + len(kwargs_list)))
             for kwargs in kwargs_list:
                 item = {"id":id, **kwargs}
                 item_list.append(item)
@@ -902,7 +902,7 @@ class DiffDatabaseMapping(DatabaseMapping):
             id = max_id + 1 if max_id else 1
         try:
             item_list = list()
-            id_list = set(range(id, len(kwargs_list)))
+            id_list = set(range(id, id + len(kwargs_list)))
             for kwargs in kwargs_list:
                 item = {"id":id, **kwargs}
                 item_list.append(item)
@@ -1365,44 +1365,42 @@ class DiffDatabaseMapping(DatabaseMapping):
             self.session.add(commit)
             self.session.flush()
             # Add new
-            new_items = list()
+            new_items = {om:[] for om in [self.ObjectClass, self.Object, 
+                                          self.RelationshipClass, self.Relationship, 
+                                          self.Parameter, self.ParameterValue]}
             for item in self.session.query(self.DiffObjectClass).\
                     filter(self.DiffObjectClass.id.in_(self.new_item_id["object_class"])):
                 kwargs = attr_dict(item)
                 kwargs['commit_id'] = commit.id
-                new_item = self.ObjectClass(**kwargs)
-                new_items.append(new_item)
+                new_items[self.ObjectClass].append(kwargs)
             for item in self.session.query(self.DiffObject).\
                     filter(self.DiffObject.id.in_(self.new_item_id["object"])):
                 kwargs = attr_dict(item)
                 kwargs['commit_id'] = commit.id
-                new_item = self.Object(**kwargs)
-                new_items.append(new_item)
+                new_items[self.Object].append(kwargs)
             for item in self.session.query(self.DiffRelationshipClass).\
                     filter(self.DiffRelationshipClass.id.in_(self.new_item_id["relationship_class"])):
                 kwargs = attr_dict(item)
                 kwargs['commit_id'] = commit.id
-                new_item = self.RelationshipClass(**kwargs)
-                new_items.append(new_item)
+                new_items[self.RelationshipClass].append(kwargs)
             for item in self.session.query(self.DiffRelationship).\
                     filter(self.DiffRelationship.id.in_(self.new_item_id["relationship"])):
                 kwargs = attr_dict(item)
                 kwargs['commit_id'] = commit.id
-                new_item = self.Relationship(**kwargs)
-                new_items.append(new_item)
+                new_items[self.Relationship].append(kwargs)
             for item in self.session.query(self.DiffParameter).\
                     filter(self.DiffParameter.id.in_(self.new_item_id["parameter"])):
                 kwargs = attr_dict(item)
                 kwargs['commit_id'] = commit.id
-                new_item = self.Parameter(**kwargs)
-                new_items.append(new_item)
+                new_items[self.Parameter].append(kwargs)
             for item in self.session.query(self.DiffParameterValue).\
                     filter(self.DiffParameterValue.id.in_(self.new_item_id["parameter_value"])):
                 kwargs = attr_dict(item)
                 kwargs['commit_id'] = commit.id
-                new_item = self.ParameterValue(**kwargs)
-                new_items.append(new_item)
-            self.session.add_all(new_items)
+                new_items[self.ParameterValue].append(kwargs)
+            # Bulk insert
+            for k, v in new_items.items():
+                self.session.bulk_insert_mappings(k, v)
             # Merge dirty
             dirty_items = list()
             for item in self.session.query(self.DiffObjectClass).\