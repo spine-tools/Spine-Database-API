37b0f1cd5e5abbbdae84410397a8295ee157fc47
Manuel Marin <manuelma@kth.se>
2018-11-13 16:14:40 +0100

    Use a better method to get the version, the other one requires importing modules which may not be there at installation time

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from setuptools import setup, find_packages
-import spinedatabase_api
+
+version = {}
+with open("spinedatabase_api/version.py") as fp:
+    exec(fp.read(), version)
 
 setup(
     name='spinedatabase_api',
-    version=spinedatabase_api.__version__,
+    version=version['__version__'],
     description='An API to talk to Spine databases',
     url='https://github.com/Spine-project/Spine-Database-API',
     author='Manuel Marin, Per Vennström',
--- spinedatabase_api/__init__.py
+++ spinedatabase_api/__init__.py
@@ -3,7 +3,6 @@ from .diff_database_mapping import DiffDatabaseMapping
 from .exception import SpineDBAPIError, SpineIntegrityError, SpineTableNotFoundError, \
     RecordNotFoundError, ParameterValueError
 from .helpers import create_new_spine_database, copy_database, merge_database, is_unlocked, OBJECT_CLASS_NAMES
+from .version import __version__
 
 name = "spinedatabase_api"
-
-__version__ = "0.0.11"
