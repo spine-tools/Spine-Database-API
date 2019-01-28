aec5dec0994eda1694a28f52f8b347e7285645a0
Manuel Marin <manuelma@kth.se>
2019-01-28 10:11:01 -0400

    Issue #38 updated: fix migration to use parameter_tag approach

--- spinedatabase_api/alembic/versions/51fd7b69acf7_add_parameter_class_and_parameter_.py
+++ spinedatabase_api/alembic/versions/51fd7b69acf7_add_parameter_class_and_parameter_.py
@@ -18,18 +18,17 @@ depends_on = None
 
 def upgrade():
     op.create_table(
-        'parameter_flag',
+        'parameter_tag',
         sa.Column('id', sa.Integer, primary_key=True),
         sa.Column('name', sa.String(155), nullable=False),
         sa.Column('description', sa.Unicode(255)),
         sa.Column('commit_id', sa.Integer, sa.ForeignKey('commit.id'), nullable=False)
     )
     op.create_table(
-        'parameter_class',
+        'parameter_definition_tag',
         sa.Column('id', sa.Integer, nullable=False),
-        sa.Column('name', sa.String(155), nullable=False),
-        sa.Column('description', sa.Unicode(255)),
-        sa.Column('flag_id', sa.Integer, sa.ForeignKey('parameter_flag.id'),),
+        sa.Column('parameter_definition_id', sa.Integer, sa.ForeignKey('parameter_definition.id'), nullable=False),
+        sa.Column('parameter_tag_id', sa.Integer, sa.ForeignKey('parameter_tag.id'), nullable=False),
         sa.Column('commit_id', sa.Integer, sa.ForeignKey('commit.id'), nullable=False)
     )
     op.create_table(
@@ -40,16 +39,13 @@ def upgrade():
         sa.Column('commit_id', sa.Integer, sa.ForeignKey('commit.id'), nullable=False)
     )
     with op.batch_alter_table("parameter_definition") as batch_op:
-        batch_op.add_column(sa.Column('class_id', sa.Integer, sa.ForeignKey(
-            'parameter_class.id', name='fk_parameter_definition_class_id_parameter_class')))
         batch_op.add_column(sa.Column('enum_id', sa.Integer, sa.ForeignKey(
             'parameter_enum.id', name='fk_parameter_definition_enum_id_parameter_enum')))
 
 
 def downgrade():
     with op.batch_alter_table("parameter_definition") as batch_op:
-        batch_op.drop_column('class_id')
         batch_op.drop_column('enum_id')
     op.drop_table('parameter_enum')
-    op.drop_table('parameter_class')
-    op.drop_table('parameter_flag')
+    op.drop_table('parameter_definition_tag')
+    op.drop_table('parameter_tag')
