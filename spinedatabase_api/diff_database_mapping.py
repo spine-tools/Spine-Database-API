9080ea34602b5f48c0156305cf0eb2be8329e679
Manuel Marin <manuelma@kth.se>
2019-03-19 07:31:03 -0400

    Reduce maximum number of sql variables on commit.

--- spinedatabase_api/diff_database_mapping.py
+++ spinedatabase_api/diff_database_mapping.py
@@ -3096,7 +3096,7 @@ class DiffDatabaseMapping(DatabaseMapping):
             commit = self.Commit(comment=comment, date=date, user=user)
             self.session.add(commit)
             self.session.flush()
-            n = 999  # Maximum number of sql variables
+            n = 499  # Maximum number of sql variables
             # Remove removed
             removed_object_class_id = list(self.removed_item_id["object_class"])
             removed_object_id = list(self.removed_item_id["object"])
