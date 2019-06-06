fc46497aa2734b41de7ac5e35b687b718eb8f9d7
Manuel Marin <manuelma@kth.se>
2019-06-06 18:29:48 -0400

    hmmmm

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -9,4 +9,4 @@ set -e
 #
 pwd
 # commit the assets in docs/build/ to the gh-pages branch and push to GitHub using SSH
-./node_modules/.bin/gh-pages -d docs/build/ -b gh-pages -r git@github.com:${TRAVIS_REPO_SLUG}.git
+#./node_modules/.bin/gh-pages -d docs/build/ -b gh-pages -r git@github.com:${TRAVIS_REPO_SLUG}.git
