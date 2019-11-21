a123fd33353bba4e7571dd350d8d739d9ae86de4
Per Vennström <per.vennstrom@vtt.fi>
2019-11-21 13:18:23 +0200

    Remove rows with empty values when importing a mapping
    When importing a mapping with timeseries or array like data
    empty values would cause problems. These are now removed
    when converting to parameter values

--- spinedb_api/json_mapping.py
+++ spinedb_api/json_mapping.py
@@ -1420,19 +1420,28 @@ def read_with_mapping(data_source, mapping, num_cols, data_header=None, column_t
             new = []
             if "time series" in k:
                 for keys, values in itertools.groupby(v, key=lambda x: x[:-1]):
-                    values = list(values)
-                    indexes = [items[-1][0] for items in values]
-                    values = [items[-1][1] for items in values]
-                    new.append(keys + (TimeSeriesVariableResolution(indexes, values, False, False),))
+                    values = [items[-1] for items in values if all(i is not None for i in items[-1])]
+                    if values:
+                        indexes = [items[0] for items in values]
+                        values = [items[1] for items in values]
+                        new.append(keys + (TimeSeriesVariableResolution(indexes, values, False, False),))
             if "time pattern" in k:
                 for keys, values in itertools.groupby(v, key=lambda x: x[:-1]):
-                    values = list(values)
-                    indexes = [items[-1][0] for items in values]
-                    values = [items[-1][1] for items in values]
-                    new.append(keys + (TimePattern(indexes, values),))
-            if "1d array" in k or "2d array" in k:
+                    values = [items[-1] for items in values if all(i is not None for i in items[-1])]
+                    if values:
+                        indexes = [items[0] for items in values]
+                        values = [items[1] for items in values]
+                        new.append(keys + (TimePattern(indexes, values),))
+            if "1d array" in k:
                 for keys, values in itertools.groupby(v, key=lambda x: x[:-1]):
-                    new.append(keys + ([items[-1] for items in values],))
+                    values = [value[-1] for value in values if value[-1] is not None]
+                    if values:
+                        new.append(keys + (values,))
+            if "2d array" in k:
+                for keys, values in itertools.groupby(v, key=lambda x: x[:-1]):
+                    values = [value[-1] for value in values if all(v is not None in value[-1])]
+                    if values:
+                        new.append(keys + (values,))
 
             if "object_parameter_values" in k:
                 if "object_parameter_values" in new_data:
