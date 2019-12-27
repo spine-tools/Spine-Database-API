9ccbf04536ab7e35de0753a53edd5d3f04272c38
jhjiang <jiangyi.huang@vtt.fi>
2019-12-27 11:07:40 +0200

    Spine Database template update

--- spinedb_api/helpers.py
+++ spinedb_api/helpers.py
@@ -695,7 +695,7 @@ def add_specifc_data_structure_for_spine_model(db_url):
             ("unit", "avail_factor", 1),
             ("unit", "min_down_time", None),
             ("unit", "min_up_time", None),
-            ("unit", "online_variable_type", "no_online_variable"),
+            ("unit", "online_variable_type", "integer"),
             ("unit", "fix_units_on", None),
             ("temporal_block", "block_start", None),
             ("temporal_block", "block_end", None),
