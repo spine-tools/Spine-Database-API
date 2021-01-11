f0ed92cdc41b4faad8368bee9dc6a3858f8efc05
Manuel Marin <manuelma@kth.se>
2021-01-11 18:24:22 +0100

    Force the naming_convention in the MySQL upgrade script

--- spinedb_api/alembic/versions/fbb540efbf15_add_support_for_mysql.py
+++ spinedb_api/alembic/versions/fbb540efbf15_add_support_for_mysql.py
@@ -7,7 +7,7 @@ Create Date: 2021-01-05 10:40:16.720937
 """
 from alembic import op
 from sqlalchemy import Text
-from spinedb_api.helpers import LONGTEXT_LENGTH
+from spinedb_api.helpers import LONGTEXT_LENGTH, naming_convention
 
 
 # revision identifiers, used by Alembic.
@@ -19,7 +19,7 @@ depends_on = None
 
 def upgrade():
     # 1. Drop referential actions in foreign keys with columns also having a check contraint
-    with op.batch_alter_table("object_class") as batch_op:
+    with op.batch_alter_table("object_class", naming_convention=naming_convention) as batch_op:
         batch_op.drop_constraint('fk_object_class_entity_class_id_entity_class', type_='foreignkey')
         batch_op.create_foreign_key(
             op.f('fk_object_class_entity_class_id_entity_class'),
@@ -27,7 +27,7 @@ def upgrade():
             ['entity_class_id', 'type_id'],
             ['id', 'type_id'],
         )
-    with op.batch_alter_table("relationship_class") as batch_op:
+    with op.batch_alter_table("relationship_class", naming_convention=naming_convention) as batch_op:
         batch_op.drop_constraint('fk_relationship_class_entity_class_id_entity_class', type_='foreignkey')
         batch_op.create_foreign_key(
             op.f('fk_relationship_class_entity_class_id_entity_class'),
@@ -35,7 +35,7 @@ def upgrade():
             ['entity_class_id', 'type_id'],
             ['id', 'type_id'],
         )
-    with op.batch_alter_table("relationship_entity_class") as batch_op:
+    with op.batch_alter_table("relationship_entity_class", naming_convention=naming_convention) as batch_op:
         batch_op.drop_constraint('fk_relationship_entity_class_member_class_id_entity_class', type_='foreignkey')
         batch_op.create_foreign_key(
             op.f('fk_relationship_entity_class_member_class_id_entity_class'),
@@ -43,46 +43,46 @@ def upgrade():
             ['member_class_id', 'member_class_type_id'],
             ['id', 'type_id'],
         )
-    with op.batch_alter_table("object") as batch_op:
+    with op.batch_alter_table("object", naming_convention=naming_convention) as batch_op:
         batch_op.drop_constraint('fk_object_entity_id_entity', type_='foreignkey')
         batch_op.create_foreign_key(
             op.f('fk_object_entity_id_entity'), 'entity', ['entity_id', 'type_id'], ['id', 'type_id']
         )
-    with op.batch_alter_table("relationship") as batch_op:
+    with op.batch_alter_table("relationship", naming_convention=naming_convention) as batch_op:
         batch_op.drop_constraint('fk_relationship_entity_id_entity', type_='foreignkey')
         batch_op.create_foreign_key(
             op.f('fk_relationship_entity_id_entity'), 'entity', ['entity_id', 'type_id'], ['id', 'type_id'],
         )
     # 2. Add new unique constraints required to make some foreign keys work
-    with op.batch_alter_table("relationship_entity_class") as batch_op:
+    with op.batch_alter_table("relationship_entity_class", naming_convention=naming_convention) as batch_op:
         batch_op.create_unique_constraint(
             'uq_relationship_entity_class', ['entity_class_id', 'dimension', 'member_class_id'],
         )
-    with op.batch_alter_table("relationship") as batch_op:
+    with op.batch_alter_table("relationship", naming_convention=naming_convention) as batch_op:
         batch_op.create_unique_constraint(
             op.f('uq_relationship_entity_identity_class_id'), ['entity_id', 'entity_class_id']
         )
-    with op.batch_alter_table("parameter_definition") as batch_op:
+    with op.batch_alter_table("parameter_definition", naming_convention=naming_convention) as batch_op:
         batch_op.create_unique_constraint(
             op.f('uq_parameter_definition_idparameter_value_list_id'), ['id', 'parameter_value_list_id'],
         )
-    with op.batch_alter_table("feature") as batch_op:
+    with op.batch_alter_table("feature", naming_convention=naming_convention) as batch_op:
         batch_op.create_unique_constraint(
             op.f('uq_feature_idparameter_value_list_id'), ['id', 'parameter_value_list_id']
         )
-    with op.batch_alter_table("tool_feature") as batch_op:
+    with op.batch_alter_table("tool_feature", naming_convention=naming_convention) as batch_op:
         batch_op.create_unique_constraint(
             op.f('uq_tool_feature_idparameter_value_list_id'), ['id', 'parameter_value_list_id']
         )
     # 3. Rename constraints having too long name
-    with op.batch_alter_table("parameter_definition_tag") as batch_op:
+    with op.batch_alter_table("parameter_definition_tag", naming_convention=naming_convention) as batch_op:
         batch_op.create_unique_constraint(
             'uq_parameter_definition_tag', ['parameter_definition_id', 'parameter_tag_id']
         )
         batch_op.drop_constraint(
             'uq_parameter_definition_tag_parameter_definition_idparameter_tag_id', type_='unique',
         )
-    with op.batch_alter_table("parameter_value") as batch_op:
+    with op.batch_alter_table("parameter_value", naming_convention=naming_convention) as batch_op:
         batch_op.create_unique_constraint(
             'uq_parameter_value', ['parameter_definition_id', 'entity_id', 'alternative_id']
         )
