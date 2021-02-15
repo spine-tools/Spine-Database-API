b9dcceabca94d89b93ac38b2d1424a1e8e9ebf8a
Manuel Marin <manuelma@kth.se>
2021-02-15 13:49:19 +0100

    Move spine_io package here and bump version
    Re Spine-project/Spine-Toolbox#956

--- spinedb_api/spine_io/__init__.py
+++ spinedb_api/spine_io/__init__.py
@@ -1,11 +1,17 @@
 ######################################################################################################################
 # Copyright (C) 2017-2021 Spine project consortium
-# This file is part of Spine Database API.
-# Spine Database API is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
-# General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
-# option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# This file is part of Spine Engine.
+# Spine Engine is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General
+# Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
+# any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
 # without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General
 # Public License for more details. You should have received a copy of the GNU Lesser General Public License along with
 # this program. If not, see <http://www.gnu.org/licenses/>.
 ######################################################################################################################
-__version__ = "0.10.10"
+
+"""
+Init file for spine_io package. Intentionally empty.
+
+:author: P. Vennström (VTT)
+:date:   1.6.2019
+"""
