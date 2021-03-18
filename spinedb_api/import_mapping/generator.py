f93c9a9544ad826b92d680d429689b7401fe321c
Manuel Marin <manuelma@kth.se>
2021-03-18 15:46:51 +0100

    Apply conversion function to data in pivoted zone

--- spinedb_api/import_mapping/generator.py
+++ spinedb_api/import_mapping/generator.py
@@ -102,8 +102,8 @@ def get_mapped_data(
         # and feed that to the mapping
         start_pos = max(mapping.read_start_row, last_pivoted_row_pos)
         for i, row in enumerate(rows[start_pos:]):
+            row = _convert_row(row, column_convert_fns, start_pos + i, errors)
             regular_row = row[:last_non_pivoted_column_pos]
-            regular_row = _convert_row(regular_row, column_convert_fns, start_pos + i, errors)
             for column_pos, unpivoted_row in zip(unpivoted_column_pos, unpivoted_rows):
                 unpivoted_row = _convert_row(unpivoted_row, row_convert_fns, k, errors)
                 full_row = regular_row + unpivoted_row
