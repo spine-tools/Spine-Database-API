9ae05b832ff4da8a513e43d06abfa12f04b10dfe
Manuel Marin <manuelma@kth.se>
2021-05-26 20:04:17 +0200

    Improve migration script
    Re #129

--- spinedb_api/alembic/versions/1e4997105288_separate_type_from_value.py
+++ spinedb_api/alembic/versions/1e4997105288_separate_type_from_value.py
@@ -25,31 +25,39 @@ def upgrade():
         batch_op.add_column(sa.Column('type', sa.String(length=255), nullable=True))
     # Populate type and default_type columns
     conn = op.get_bind()
-    for row in conn.execute("SELECT id, value FROM parameter_value"):
-        val = json.loads(row.value)
-        if not isinstance(val, dict):
+    m = sa.MetaData(conn)
+    m.reflect()
+    _update_table("parameter_definition", m, conn)
+    _update_table("parameter_value", m, conn)
+
+
+def _update_table(tablename, m, conn):
+    value_field, type_field = {
+        "parameter_definition": ("default_value", "default_type"),
+        "parameter_value": ("value", "type"),
+    }[tablename]
+    table = m.tables[tablename]
+    items = []
+    for row in conn.execute(table.select()):
+        value = getattr(row, value_field, None)
+        if value is None:
             continue
-        type_ = val.pop("type", None)
+        parsed_value = json.loads(value)
+        if not isinstance(parsed_value, dict):
+            continue
+        type_ = parsed_value.pop("type", None)
         if type_ is None:
             continue
-        value = json.dumps(val)
-        conn.execute(
-            "UPDATE parameter_value SET type = :type, value = :value WHERE id = :id", type=type_, value=value, id=row.id
-        )
-    for row in conn.execute("SELECT id, default_value FROM parameter_definition"):
-        val = json.loads(row.default_value)
-        if not isinstance(val, dict):
-            continue
-        type_ = val.pop("type", None)
-        if type_ is None:
-            continue
-        value = json.dumps(val)
-        conn.execute(
-            "UPDATE parameter_definition SET default_type = :type, default_value = :value WHERE id = :id",
-            type=type_,
-            value=value,
-            id=row.id,
-        )
+        value = json.dumps(parsed_value)
+        items.append({"b_id": row.id, value_field: value, type_field: type_})
+    if not items:
+        return
+    upd = (
+        table.update()
+        .where(table.c.id == sa.bindparam('b_id'))
+        .values(**{value_field: sa.bindparam(value_field), type_field: sa.bindparam(type_field)})
+    )
+    conn.execute(upd, items)
 
 
 def downgrade():
