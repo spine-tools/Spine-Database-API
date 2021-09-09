cf13d0abaf83919b0411fec4f45a58ce1f8a5940
Pekka T Savolainen <pekka.t.savolainen@vtt.fi>
2021-09-09 10:08:45 +0300

    Copyright corrections

--- spinedb_api/export_functions.py
+++ spinedb_api/export_functions.py
@@ -1,6 +1,6 @@
 ######################################################################################################################
 # Copyright (C) 2017-2021 Spine project consortium
-# This file is part of Spine Toolbox.
+# This file is part of Spine Database API.
 # Spine Toolbox is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General
 # Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
 # any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
