dc46639e8c6d39a45870bf46d11563dbf5fadabf
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2023-03-30 23:42:57 +0200

    tests/test_check_functions.py: update as per code review
    - all numbers are floats
    - use descriptive names

--- tests/test_check_functions.py
+++ tests/test_check_functions.py
@@ -10,6 +10,7 @@
 ######################################################################################################################
 
 import json
+from numbers import Number
 import unittest
 
 from spinedb_api.db_cache import DBCache, ParameterValueItem
@@ -22,17 +23,17 @@ class TestCheckFunctions(unittest.TestCase):
     def setUp(self):
         self.data = [
             (bool, (b'"TRUE"', b'"FALSE"', b'"T"', b'"True"', b'"False"'), (b'true', b'false')),
-            (int, (b'32',), (b'42', b'-2')),
+            (int, (b'32', b'3.14'), (b'42', b'-2')),
             (str, (b'"FOO"', b'"bar"'), (b'"foo"', b'"Bar"', b'"BAZ"')),
         ]
-        self.par_defns = {
+        self.parameter_definitions = {
             1: {'name': 'par1', 'entity_class_id': 1, 'parameter_value_list_id': 1},
             2: {'name': 'par2', 'entity_class_id': 1, 'parameter_value_list_id': 2},
             3: {'name': 'par2', 'entity_class_id': 1, 'parameter_value_list_id': 3},
         }
         self.value_type = {bool: 1, int: 2, str: 3}
-        self.par_val_lists = {1: (1, 2), 2: (3, 4), 3: (5, 6, 7)}
-        self.list_vals = {1: True, 2: False, 3: 42, 4: -2, 5: 'foo', 6: 'Bar', 7: 'BAZ'}
+        self.parameter_value_lists = {1: (1, 2), 2: (3, 4), 3: (5, 6, 7)}
+        self.list_values = {1: True, 2: False, 3: 42, 4: -2, 5: 'foo', 6: 'Bar', 7: 'BAZ'}
 
     def get_item(self, _type: type, val: bytes):
         _id = self.value_type[_type]  # setup: param defn/value list ids are equal
@@ -56,22 +57,26 @@ class TestCheckFunctions(unittest.TestCase):
         for _type, _fail, _pass in self.data:
             for data in _fail:
                 with self.subTest(_type=_type, data=data):
-                    # expect_in = f"{json.loads(data.decode('utf8'))}"
                     expect_in = json.loads(data.decode('utf8'))
-                    ref = [self.list_vals[i] for i in self.par_val_lists[self.value_type[_type]]]
+                    if isinstance(expect_in, Number):
+                        expect_in = float(expect_in)
+                    ref = [self.list_values[i] for i in self.parameter_value_lists[self.value_type[_type]]]
                     expect_ref = ", ".join(f"{json.dumps(i)!r}" for i in ref)
                     self.assertRaisesRegex(
                         SpineIntegrityError,
                         fr"{expect_in!r}.+{expect_ref}",
                         replace_parameter_values_with_list_references,
                         self.get_item(_type, data),
-                        self.par_defns,
-                        self.par_val_lists,
-                        self.list_vals,
+                        self.parameter_definitions,
+                        self.parameter_value_lists,
+                        self.list_values,
                     )
 
             for data in _pass:
                 with self.subTest(_type=_type, data=data):
                     replace_parameter_values_with_list_references(
-                        self.get_item(_type, data), self.par_defns, self.par_val_lists, self.list_vals
+                        self.get_item(_type, data),
+                        self.parameter_definitions,
+                        self.parameter_value_lists,
+                        self.list_values,
                     )
