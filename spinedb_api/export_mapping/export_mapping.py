a1dd5f2b1534c82af1a171d15a63ec57eee6914b
Antti Soininen <antti.soininen@vtt.fi>
2023-10-06 14:51:47 +0300

    Fix MAP_TYPE
    Re #279

--- spinedb_api/export_mapping/export_mapping.py
+++ spinedb_api/export_mapping/export_mapping.py
@@ -1004,7 +1004,7 @@ class ParameterDefaultValueTypeMapping(ParameterDefaultValueMapping):
     an :class:`AlternativeMapping` as parents.
     """
 
-    MAP_TYPE = "ParameterValueType"
+    MAP_TYPE = "ParameterDefaultValueType"
 
     def _data(self, db_row):
         type_ = db_row.default_type
