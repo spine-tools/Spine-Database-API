111f584c69ca1dec60e1f3ad7eb19e01f1259fa7
Antti Soininen <antti.soininen@vtt.fi>
2025-02-28 10:20:20 +0200

    Fix export_entities()
    export_entities() was writing element_name_list to the output
    while import_entities() requires entity_byname instead.

--- spinedb_api/export_functions.py
+++ spinedb_api/export_functions.py
@@ -164,7 +164,7 @@ def export_entity_class_display_modes(db_map, ids=Asterisk):
 def export_entities(db_map, ids=Asterisk):
     return sorted(
         (
-            (x.entity_class_name, x.element_name_list or x.name, x.description)
+            (x.entity_class_name, x.entity_byname if x.element_name_list else x.name, x.description)
             for x in _get_items(db_map, "entity", ids)
         ),
         key=lambda x: (0 if isinstance(x[1], str) else len(x[1]), x[0], (x[1],) if isinstance(x[1], str) else x[1]),
