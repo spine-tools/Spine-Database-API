a7f2140bf58a6ec19a547d9dd3ca9b2f74c5c8f5
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2025-05-26 15:41:54 +0200

    alembic: blacken reencode_parameter_values.py

--- spinedb_api/alembic/versions/a973ab537da2_reencode_parameter_values.py
+++ spinedb_api/alembic/versions/a973ab537da2_reencode_parameter_values.py
@@ -5,14 +5,15 @@ Revises: 91f1f55aa972
 Create Date: 2025-05-21 12:49:16.861670
 
 """
+
 from alembic import op
 import sqlalchemy as sa
 from spinedb_api.compat.reencode_for_data_transition import transition_data
 
 
 # revision identifiers, used by Alembic.
-revision = 'a973ab537da2'
-down_revision = '91f1f55aa972'
+revision = "a973ab537da2"
+down_revision = "91f1f55aa972"
 branch_labels = None
 depends_on = None
 
@@ -25,10 +26,11 @@ def upgrade():
 
     # Define a lightweight representation of the table
     my_table = sa.Table(
-        'parameter_value', metadata,
-        sa.Column('id', sa.Integer, primary_key=True),
-        sa.Column('value', sa.BINARY),
-        sa.Column('type', sa.String),  # TODO do we need the type?
+        "parameter_value",
+        metadata,
+        sa.Column("id", sa.Integer, primary_key=True),
+        sa.Column("value", sa.BINARY),
+        sa.Column("type", sa.String),  # TODO do we need the type?
     )
 
     # Read current data
@@ -40,9 +42,7 @@ def upgrade():
         new_value = transition_data(old_value)
 
         # Update the row
-        conn.execute(
-            my_table.update().where(my_table.c.id == row.id).values(value=new_value)
-        )
+        conn.execute(my_table.update().where(my_table.c.id == row.id).values(value=new_value))
 
 
 def downgrade():
