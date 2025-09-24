43ec1d244a621e1c437ee1f288629504d245cd06
Antti Soininen <antti.soininen@vtt.fi>
2025-09-24 12:35:31 +0300

    Add more Alembic migration tests

--- tests/test_alembic_migration.py
+++ tests/test_alembic_migration.py
@@ -12,6 +12,7 @@
 import pathlib
 import shutil
 from spinedb_api import (
+    Array,
     DatabaseMapping,
     DateTime,
     Duration,
@@ -23,7 +24,8 @@ from spinedb_api import (
 
 
 class TestMigrationFrom070a0eb89e88:
-    def test_from_070a0eb89e88_to_latest(self, tmp_path):
+    # Test migration from a very old revision (e.g. no alternatives or object groups yet).
+    def test_migration_to_latest(self, tmp_path):
         source_path = pathlib.Path(__file__).parent / "legacy_databases" / "070a0eb89e88.sqlite"
         destination_path = tmp_path / "db.sqlite"
         shutil.copyfile(source_path, destination_path)
@@ -66,17 +68,14 @@ class TestMigrationFrom070a0eb89e88:
         entity_classes = db_map.find_entity_classes()
         assert len(entity_classes) == 3
         widget_class = db_map.entity_class(name="Widget")
-        assert widget_class["name"] == "Widget"
         assert widget_class["description"] == "Widgets of all kinds."
         assert widget_class["dimension_name_list"] == ()
         assert widget_class["display_icon"] == 280379751657962
         gadget_class = db_map.entity_class(name="Gadget")
-        assert gadget_class["name"] == "Gadget"
         assert gadget_class["description"] == "Gadget is not widget."
         assert gadget_class["dimension_name_list"] == ()
         assert gadget_class["display_icon"] == 280741980139948
         gadget_widget_class = db_map.entity_class(name="Gadget__Widget")
-        assert gadget_widget_class["name"] == "Gadget__Widget"
         assert gadget_widget_class["description"] is None
         assert gadget_widget_class["dimension_name_list"] == ("Gadget", "Widget")
         assert gadget_widget_class["display_icon"] is None
@@ -86,23 +85,18 @@ class TestMigrationFrom070a0eb89e88:
         entities = db_map.find_entities()
         assert len(entities) == 5
         router = db_map.entity(entity_class_name="Gadget", name="router")
-        assert router["name"] == "router"
         assert router["description"] is None
         assert router["element_name_list"] == ()
         mouse = db_map.entity(entity_class_name="Gadget", name="mouse")
-        assert mouse["name"] == "mouse"
         assert mouse["description"] is None
         assert mouse["element_name_list"] == ()
         calculator = db_map.entity(entity_class_name="Widget", name="calculator")
-        assert calculator["name"] == "calculator"
         assert calculator["description"] == "Standard phone calculator."
         assert calculator["element_name_list"] == ()
         clock = db_map.entity(entity_class_name="Widget", name="clock")
-        assert clock["name"] == "clock"
         assert clock["description"] == "Fancy digital clock."
         assert clock["element_name_list"] == ()
         router_calculator = db_map.entity(entity_class_name="Gadget__Widget", entity_byname=("router", "calculator"))
-        assert router_calculator["name"] == "router__calculator"
         assert router_calculator["description"] is None
         assert router_calculator["element_name_list"] == ("router", "calculator")
 
@@ -111,50 +105,41 @@ class TestMigrationFrom070a0eb89e88:
         parameter_definitions = db_map.find_parameter_definitions()
         assert len(parameter_definitions) == 11
         float_definition = db_map.parameter_definition(entity_class_name="Widget", name="float")
-        assert float_definition["name"] == "float"
         assert float_definition["description"] == "Parameter with float values."
         assert float_definition["parsed_value"] == 2.3
         assert float_definition["parameter_value_list_name"] is None
         null_definition = db_map.parameter_definition(entity_class_name="Widget", name="none")
-        assert null_definition["name"] == "none"
         assert null_definition["description"] == "Parameter with no values."
         assert null_definition["parsed_value"] is None
         assert null_definition["parameter_value_list_name"] is None
         value_list_definition = db_map.parameter_definition(entity_class_name="Widget", name="value_list")
-        assert value_list_definition["name"] == "value_list"
         assert value_list_definition["description"] == "Parameter with list values."
         assert value_list_definition["parsed_value"] == "red"
         assert value_list_definition["parameter_value_list_name"] == "Enumeration"
         string_definition = db_map.parameter_definition(entity_class_name="Widget", name="string")
-        assert string_definition["name"] == "string"
         assert string_definition["description"] == "Parameter with string values."
         assert string_definition["parsed_value"] == "a default string"
         assert string_definition["parameter_value_list_name"] is None
         boolean_definition = db_map.parameter_definition(entity_class_name="Widget", name="boolean")
-        assert boolean_definition["name"] == "boolean"
         assert boolean_definition["description"] == "Parameter with boolean values."
         assert isinstance(boolean_definition["parsed_value"], bool)
         assert boolean_definition["parsed_value"]
         assert boolean_definition["parameter_value_list_name"] is None
         date_time_definition = db_map.parameter_definition(entity_class_name="Widget", name="date_time")
-        assert date_time_definition["name"] == "date_time"
         assert date_time_definition["description"] == "Parameter with datetime values."
         assert date_time_definition["parsed_value"] == DateTime("2020-04-22T14:20:00")
         assert date_time_definition["parameter_value_list_name"] is None
         duration_definition = db_map.parameter_definition(entity_class_name="Widget", name="duration")
-        assert duration_definition["name"] == "duration"
         assert duration_definition["description"] == "Parameter with duration values."
         assert duration_definition["parsed_value"] == Duration("5h")
         assert duration_definition["parameter_value_list_name"] is None
         time_pattern_definition = db_map.parameter_definition(entity_class_name="Widget", name="time_pattern")
-        assert time_pattern_definition["name"] == "time_pattern"
         assert time_pattern_definition["description"] == "Parameter with time pattern values."
         assert time_pattern_definition["parsed_value"] == TimePattern(["D1-7"], [1.1])
         assert time_pattern_definition["parameter_value_list_name"] is None
         time_series_fixed_resolution_definition = db_map.parameter_definition(
             entity_class_name="Widget", name="time_series_fixed_resolution"
         )
-        assert time_series_fixed_resolution_definition["name"] == "time_series_fixed_resolution"
         assert time_series_fixed_resolution_definition["description"] == "Parameter with time series values."
         assert time_series_fixed_resolution_definition["parsed_value"] == TimeSeriesFixedResolution(
             "2020-04-22 00:00:00", "3h", [1.1, 2.2, 3.3], ignore_year=True, repeat=False
@@ -163,7 +148,6 @@ class TestMigrationFrom070a0eb89e88:
         time_series_variable_resolution_definition = db_map.parameter_definition(
             entity_class_name="Widget", name="time_series_variable_resolution"
         )
-        assert time_series_variable_resolution_definition["name"] == "time_series_variable_resolution"
         assert time_series_variable_resolution_definition["description"] == "Parameter with time series values."
         assert time_series_variable_resolution_definition["parsed_value"] == TimeSeriesVariableResolution(
             ["2000-01-01T00:00:00", "2000-01-01T01:00:00", "2000-01-01T02:00:00"],
@@ -173,7 +157,6 @@ class TestMigrationFrom070a0eb89e88:
         )
         assert time_series_variable_resolution_definition["parameter_value_list_name"] is None
         map_definition = db_map.parameter_definition(entity_class_name="Widget", name="map")
-        assert map_definition["name"] == "map"
         assert map_definition["description"] == "Parameter with map values."
         assert map_definition["parsed_value"] == Map(
             ["A", "B"], [Map(["T00", "T01"], [1.1, 2.2]), Map(["T00", "T01"], [3.3, 4.4])]
@@ -271,3 +254,148 @@ class TestMigrationFrom070a0eb89e88:
         assert map_value["parsed_value"] == Map(
             ["A", "A", "B", "B"], [-1.1, Map(["a"], [-2.2]), -3.3, Map(["b"], [-4.4])]
         )
+
+
+class TestMigrationFrom39e860a11b05:
+    # This revision includes object groups, alternatives and scenarios that were not present in 070a0eb89e88.
+    # Also, the Array value type was introduced between the revisions.
+    def test_migration_to_latest(self, tmp_path):
+        source_path = pathlib.Path(__file__).parent / "legacy_databases" / "39e860a11b05.sqlite"
+        destination_path = tmp_path / "db.sqlite"
+        shutil.copyfile(source_path, destination_path)
+        url = "sqlite:///" + str(destination_path)
+        with DatabaseMapping(url, upgrade=True) as db_map:
+            self._assert_alternatives(db_map)
+            self._assert_scenarios(db_map)
+            self._assert_entity_classes(db_map)
+            self._assert_entities(db_map)
+            self._assert_entity_groups(db_map)
+            self._assert_parameter_definitions(db_map)
+            self._assert_parameter_values(db_map)
+
+    @staticmethod
+    def _assert_alternatives(db_map):
+        alternatives = db_map.find_alternatives()
+        assert len(alternatives) == 3
+        base = db_map.alternative(name="Base")
+        assert base["description"] == "Base alternative"
+        high_greed = db_map.alternative(name="HighGreed")
+        assert high_greed["description"] == "We want your money."
+        low_greed = db_map.alternative(name="LowGreed")
+        assert low_greed["description"] == "We can do with less."
+
+    @staticmethod
+    def _assert_scenarios(db_map):
+        scenarios = db_map.find_scenarios()
+        assert len(scenarios) == 3
+        base = db_map.scenario(name="base")
+        assert base["description"] == "Base prices."
+        assert base["alternative_name_list"] == ["Base"]
+        high_cost = db_map.scenario(name="high_cost")
+        assert high_cost["description"] == "High prices."
+        assert high_cost["alternative_name_list"] == ["Base", "HighGreed"]
+        low_cost = db_map.scenario(name="low_cost")
+        assert low_cost["description"] == "Low prices."
+        assert low_cost["alternative_name_list"] == ["Base", "LowGreed"]
+
+    @staticmethod
+    def _assert_entity_classes(db_map):
+        entity_classes = db_map.find_entity_classes()
+        assert len(entity_classes) == 2
+        widget_class = db_map.entity_class(name="Widget")
+        assert widget_class["description"] is None
+        assert widget_class["dimension_name_list"] == ()
+        assert widget_class["display_icon"] == 280743406203308
+        widget_class = db_map.entity_class(name="Chain")
+        assert widget_class["description"] is None
+        assert widget_class["dimension_name_list"] == ()
+        assert widget_class["display_icon"] == 280923065020609
+
+    @staticmethod
+    def _assert_entities(db_map):
+        assert len(db_map.find_entities()) == 4
+        notepad = db_map.entity(entity_class_name="Widget", name="notepad")
+        assert notepad["description"] is None
+        wallet = db_map.entity(entity_class_name="Widget", name="wallet")
+        assert wallet["description"] is None
+        open_source = db_map.entity(entity_class_name="Widget", name="open_source")
+        assert open_source["description"] is None
+        links = db_map.entity(entity_class_name="Chain", name="links")
+        assert links["description"] is None
+
+    @staticmethod
+    def _assert_entity_groups(db_map):
+        group = db_map.find_entity_groups(entity_class_name="Widget", group_name="open_source")
+        assert len(group) == 1
+        group_item = group[0]
+        assert group_item["member_name"] == "notepad"
+
+    @staticmethod
+    def _assert_parameter_definitions(db_map):
+        assert len(db_map.find_parameter_definitions()) == 3
+        price = db_map.parameter_definition(entity_class_name="Widget", name="price")
+        assert price["description"] is None
+        assert price["parsed_value"] is None
+        assert price["parameter_value_list_name"] is None
+        link_strength = db_map.parameter_definition(entity_class_name="Chain", name="link_strength")
+        assert link_strength["description"] is None
+        assert link_strength["parsed_value"] == Array([1.1, 2.2])
+        assert link_strength["parameter_value_list_name"] is None
+        yield_time = db_map.parameter_definition(entity_class_name="Chain", name="yield_time")
+        assert yield_time["description"] is None
+        assert yield_time["parsed_value"] == Array([Duration("2h"), Duration("3h")])
+        assert yield_time["parameter_value_list_name"] is None
+
+    @staticmethod
+    def _assert_parameter_values(db_map):
+        assert (len(db_map.find_parameter_values())) == 6
+        assert (
+            db_map.parameter_value(
+                entity_class_name="Widget",
+                entity_byname=("notepad",),
+                parameter_definition_name="price",
+                alternative_name="Base",
+            )["parsed_value"]
+            == 0.0
+        )
+        assert (
+            db_map.parameter_value(
+                entity_class_name="Widget",
+                entity_byname=("wallet",),
+                parameter_definition_name="price",
+                alternative_name="Base",
+            )["parsed_value"]
+            == 2.2
+        )
+        assert (
+            db_map.parameter_value(
+                entity_class_name="Widget",
+                entity_byname=("wallet",),
+                parameter_definition_name="price",
+                alternative_name="HighGreed",
+            )["parsed_value"]
+            == 3.3
+        )
+        assert (
+            db_map.parameter_value(
+                entity_class_name="Widget",
+                entity_byname=("wallet",),
+                parameter_definition_name="price",
+                alternative_name="LowGreed",
+            )["parsed_value"]
+            == 1.1
+        )
+        link_strength = db_map.parameter_value(
+            entity_class_name="Chain",
+            entity_byname=("links",),
+            parameter_definition_name="link_strength",
+            alternative_name="Base",
+        )
+        assert link_strength["parsed_value"] == Array([-1.1, -2.2])
+        yield_time = db_map.parameter_value(
+            entity_class_name="Chain",
+            entity_byname=("links",),
+            parameter_definition_name="yield_time",
+            alternative_name="Base",
+        )
+        assert yield_time["parsed_value"] == Array([Duration("2M"), Duration("3M")])
