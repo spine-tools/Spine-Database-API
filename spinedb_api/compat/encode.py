3ad53f6fd3fab1d38dceeb7f38f3c11de13bb0ff
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2025-11-19 20:07:41 +0100

    compat/encode.py: hormanise encoders w/ other utilities in models.py

--- spinedb_api/compat/encode.py
+++ spinedb_api/compat/encode.py
@@ -15,20 +15,9 @@ from datetime import datetime
 import enum
 from itertools import chain
 from types import NoneType
-from typing import Any, Sequence, TypeVar
-from dateutil.relativedelta import relativedelta
-import pandas as pd
-from ..models import (
-    AnyArray,
-    Array,
-    ArrayIndex,
-    DictEncodedArray,
-    DictEncodedIndex,
-    RunEndArray,
-    RunEndIndex,
-    Table,
-    type_map,
-)
+from typing import Any, Sequence, cast
+
+from ..models import TypeNames, type_map, ArrayAsDict
 
 
 def convert_records_to_columns(recs: list[dict[str, Any]]) -> dict[str, list]:
@@ -40,13 +29,24 @@ def convert_records_to_columns(recs: list[dict[str, Any]]) -> dict[str, list]:
     return columns
 
 
+def types_w_none(arr: Sequence) -> tuple[list[type], bool]:
+    all_types = set(map(type, arr))
+    has_none = NoneType in all_types
+    return list(all_types - {NoneType}), has_none
+
+
 _sentinel = enum.Enum("_sentinel", "value")
 SENTINEL = _sentinel.value
 
-re_t = TypeVar("re_t", RunEndArray, RunEndIndex)
+
+def _get_value_type(values) -> TypeNames:
+    types_, _ = types_w_none(values)
+    if len(types_) > 1:
+        raise ValueError(f"array has mixed types: {types_}")
+    return cast(TypeNames, type_map[types_[0]])
 
 
-def re_encode(name: str, vals: list, array_t: type[re_t]) -> re_t:
+def re_encode(name: str, vals: list) -> ArrayAsDict:
     last = SENTINEL
     values, run_end = [], []
     for idx, val in enumerate(vals, start=1):
@@ -56,52 +56,27 @@ def re_encode(name: str, vals: list, array_t: type[re_t]) -> re_t:
         else:
             run_end[-1] = idx
         last = val
-    return array_t(name=name, values=values, run_end=run_end)
+
+    res: ArrayAsDict = {
+        "name": name,
+        "run_end": run_end,
+        "values": values,
+        "value_type": _get_value_type(values),
+        "type": "run_end_array",
+    }
+    return res
 
 
-de_t = TypeVar("de_t", DictEncodedArray, DictEncodedIndex)
-
-
-def de_encode(name: str, value_type: str, vals: list, array_t: type[de_t]) -> de_t:
+def de_encode(name: str, vals: list) -> ArrayAsDict:
     # not using list(set(...)) to preserve order
     values = list(dict.fromkeys(vals))
     indices = list(map(values.index, vals))
-    return array_t(name=name, value_type=value_type, values=values, indices=indices)
 
-
-def is_any_w_none(arr: Sequence) -> tuple[bool, bool]:
-    all_types = set(map(type, arr))
-    has_none = NoneType in all_types
-    return len(all_types - {NoneType}) > 1, has_none
-
-
-def to_array(name: str, col: list):
-    any_type, has_none = is_any_w_none(col)
-    if any_type:
-        return AnyArray(name=name, values=col)
-
-    match name, col, has_none:
-        case "value", list(), _:
-            return Array(name=name, value_type=type_map[type(col[0])], values=col)
-        case _, [float(), *_], _:
-            return Array(name=name, value_type="float", values=col)
-        case _, [bool(), *_], _:
-            return Array(name=name, value_type="bool", values=col)
-        case _, [int(), *_], True:
-            return Array(name=name, value_type="int", values=col)
-        case _, [int(), *_], False:
-            return ArrayIndex(name=name, value_type="int", values=col)
-        case _, [pd.Timestamp() | datetime(), *_], False:
-            return ArrayIndex(name=name, value_type="date_time", values=col)
-        case _, [relativedelta(), *_], False:
-            return ArrayIndex(name=name, value_type="duration", values=col)
-        case _, [str(), *_], True:
-            return de_encode(name, "str", col, DictEncodedArray)
-        case _, [str(), *_], False:
-            return de_encode(name, "str", col, DictEncodedIndex)
-        case _, _, _:
-            raise NotImplementedError(f"{name}: unknown column type {type(col[0])} ({has_none=})")
-
-
-def to_table(columns: dict[str, list]) -> Table:
-    return [to_array(name, col) for name, col in columns.items()]
+    res: ArrayAsDict = {
+        "name": name,
+        "indices": indices,
+        "values": values,
+        "value_type": _get_value_type(values),
+        "type": "dict_encoded_array",
+    }
+    return res
